1969d7b81b78a177cb8c3449995e81464f676ff5
Max Vilà Ruiz <max16506@gmail.com>
2025-07-09 17:45:45 +0200

    Add fenster.h defining cross-platform window struct (macOS, Windows, X11)

---
+++ fenster.h
@@ -0,0 +1,36 @@
+#ifndef FENSTER_H
+#define FENSTER_H
+
+#if defined(__APPLE__)
+#include <CoreGraphics/CoreGraphics.h>
+#include <objc/NSObjCRuntime.h>
+#include <objc/objc-runtime.h>
+#elif defined(_WIN32)
+#endif
+
+#include <stdint.h>
+#include <stdlib.h>
+#include <string.h>
+
+struct fenster {
+    const char *title;
+    const int   width;
+    const int   height;
+    uint32_t   *buf;
+    int         keys[256]; /* keys are mostly ASCII, but arrows are 17..20 */
+    int         mod;       /* mod is 4 bits mask, ctrl=1, shift=2, alt=4, meta=8 */
+    int         x;
+    int         y;
+    int         mouse;
+#if defined(__APPLE__)
+    id wnd;
+#elif defined(_WIN32)
+    HWND hwnd;
+#else
+    Display *dpy;
+    Window   w;
+    GC       gc;
+    XImage  *img;
+#endif
+};
+#endif /* FENSTER_H */
