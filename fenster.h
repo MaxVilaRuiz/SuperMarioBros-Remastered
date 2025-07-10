07f89dbcb3e4c4f17b8638c73351b11403472e8c
Max Vilà Ruiz <max16506@gmail.com>
2025-07-10 20:16:29 +0200

    Add custom Win32 window procedure with bitmap rendering and input handling

--- fenster.h
+++ fenster.h
@@ -157,6 +157,59 @@ FENSTER_API int fenster_loop(struct fenster *f) {
 static const uint8_t FENSTER_KEYCODES[] = {0,27,49,50,51,52,53,54,55,56,57,48,45,61,8,9,81,87,69,82,84,89,85,73,79,80,91,93,10,0,65,83,68,70,71,72,74,75,76,59,39,96,0,92,90,88,67,86,66,78,77,44,46,47,0,0,0,32,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,2,17,3,0,20,0,19,0,5,18,4,26,127};
 // clang-format on
 
+typedef struct BINFO {
+    BITMAPINFOHEADER bmiHeader;
+    RGBQUAD          bmiColors[3];
+} BINFO;
+
+static LRESULT CALLBACK fenster_wndproc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam) {
+    struct fenster *f = (struct fenster *)GetWindowLongPtr(hwnd, GWLP_USERDATA);
+    switch (msg) {
+        case WM_PAINT: {
+            PAINTSTRUCT ps;
+            HDC         hdc = BeginPaint(hwnd, &ps);
+            HDC         memdc = CreateCompatibleDC(hdc);
+            HBITMAP     hbmp = CreateCompatibleBitmap(hdc, f->width, f->height);
+            HBITMAP     oldbmp = (HBITMAP)SelectObject(memdc, hbmp);
+            BINFO       bi = {{sizeof(bi), f->width, -f->height, 1, 32, BI_BITFIELDS}};
+            bi.bmiColors[0].rgbRed = 0xff;
+            bi.bmiColors[1].rgbGreen = 0xff;
+            bi.bmiColors[2].rgbBlue = 0xff;
+            SetDIBitsToDevice(memdc, 0, 0, f->width, f->height, 0, 0, 0, f->height, f->buf,
+                              (BITMAPINFO *)&bi, DIB_RGB_COLORS);
+            BitBlt(hdc, 0, 0, f->width, f->height, memdc, 0, 0, SRCCOPY);
+            SelectObject(memdc, oldbmp);
+            DeleteObject(hbmp);
+            DeleteDC(memdc);
+            EndPaint(hwnd, &ps);
+        } break;
+        case WM_CLOSE:
+            DestroyWindow(hwnd);
+            break;
+        case WM_LBUTTONDOWN:
+        case WM_LBUTTONUP:
+            f->mouse = (msg == WM_LBUTTONDOWN);
+            break;
+        case WM_MOUSEMOVE:
+            f->y = HIWORD(lParam), f->x = LOWORD(lParam);
+            break;
+        case WM_KEYDOWN:
+        case WM_KEYUP: {
+            f->mod = ((GetKeyState(VK_CONTROL) & 0x8000) >> 15) |
+                     ((GetKeyState(VK_SHIFT) & 0x8000) >> 14) |
+                     ((GetKeyState(VK_MENU) & 0x8000) >> 13) |
+                     (((GetKeyState(VK_LWIN) | GetKeyState(VK_RWIN)) & 0x8000) >> 12);
+            f->keys[FENSTER_KEYCODES[HIWORD(lParam) & 0x1ff]] = !((lParam >> 31) & 1);
+        } break;
+        case WM_DESTROY:
+            PostQuitMessage(0);
+            break;
+        default:
+            return DefWindowProc(hwnd, msg, wParam, lParam);
+    }
+    return 0;
+}
+
 #endif /* ERASE */
 #endif /* !FENSTER_HEADER */
 #endif /* FENSTER_H */
