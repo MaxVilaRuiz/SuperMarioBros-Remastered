26007f342c46bcb6e96b8c1bdb68bcac86c4ea80
Max Vilà Ruiz <max16506@gmail.com>
2025-07-13 21:11:57 +0200

    Update utils class with window class changes

--- utils.cc
+++ utils.cc
@@ -5,15 +5,13 @@ namespace pro2 {
 
 void paint_hline(Window& window, int xini, int xfin, int y, Color color) {
     for (int x = xini; x <= xfin; x++) {
-        //  TODO
-        // window.set_pixel({x, y}, color);
+        window.set_pixel({x, y}, color);
     }
 }
 
 void paint_vline(Window& window, int x, int yini, int yfin, Color color) {
     for (int y = yini; y <= yfin; y++) {
-        //  TODO
-        // window.set_pixel({x, y}, color);
+        window.set_pixel({x, y}, color);
     }
 }
 
@@ -26,8 +24,7 @@ void paint_sprite(Window&              window,
         for (int j = 0; j < line.size(); j++) {
             const int _j = mirror ? line.size() - j - 1 : j;
             if (line[_j] >= 0) {
-                // TODO
-                // window.set_pixel({orig.x + j, orig.y + i}, line[_j]);
+                window.set_pixel({orig.x + j, orig.y + i}, line[_j]);
             }
         }
     }
@@ -36,8 +33,7 @@ void paint_sprite(Window&              window,
 void paint_rect(Window& window, Rect rectangle, Color color) {
     for (int x = rectangle.left; x < rectangle.right; x++) {
         for (int y = rectangle.bottom; y < rectangle.top; y++) {
-            //  TODO
-            // window.set_pixel({x,y}, color);
+            window.set_pixel({x,y}, color);
         }
     }
 }
