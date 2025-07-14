9acd14925b7a2feebdbf3f7e8a8c09732def4f6f
Max Vilà Ruiz <max16506@gmail.com>
2025-07-14 12:38:33 +0200

    Add spatial grid management methods to Finder class

--- finder.hh
+++ finder.hh
@@ -46,4 +46,80 @@ class Finder {
             }
         }
     }
+
+ public:
+    Finder() {}
+
+    /**
+     * @brief Adds an object to the spatial grid.
+     * @param t Pointer to the object to be added.
+     * @pre t != nullptr
+     * @post Object t is registered in all grid cells overlapping its bounding box.
+     */
+    void add(const T* t) {
+        apply_action(t, true);
+    }
+
+    /**
+     * @brief Removes an object from the spatial grid.
+     * @param t Pointer to the object to be removed.
+     * @pre t != nullptr
+     * @post Object t is removed from all grid cells it was registered in.
+     */
+    void remove(const T* t) {
+        apply_action(t, false);
+    }
+
+    /**
+     * @brief Updates the position of an object in the spatial grid.
+     * @param t Pointer to the object to be updated.
+     * @pre t != nullptr
+     * @post Object t is reinserted in grid cells based on its new position.
+     */
+    void update(const T* t) {
+        remove(t);
+        add(t);
+    }
+
+    /**
+     * @brief Queries all objects currently overlapping a given rectangle.
+     * @param qrect The query rectangle.
+     * @return Set of pointers to objects that intersect qrect.
+     * @pre qrect is within the bounds of the grid.
+     * @post Returns all objects whose bounding boxes overlap qrect.
+     */
+    std::set<const T*> query(pro2::Rect qrect) const {
+        std::set<const T*> aux;
+        int left_cell = std::max(0, qrect.left / CELL_WIDTH);
+        int right_cell = std::min(MAX_COL, qrect.right / CELL_WIDTH);
+        int top_cell = std::max(0, qrect.top / CELL_HEIGHT);
+        int bottom_cell = std::min(MAX_ROW, qrect.bottom / CELL_HEIGHT);
+
+        // Gather objects from overlapping cells
+        for (int i = left_cell; i <= right_cell; i++) {
+            for (int j = top_cell; j <= bottom_cell; j++) {
+                if (objects_.count(i) && objects_.at(i).count(j)) {
+                    for (const T* obj : objects_.at(i).at(j)) aux.insert(obj);
+                }
+            }
+        }
+
+        // Filter objects that truly intersect the query rectangle
+        std::set<const T*> res;
+        for (const T* obj : aux) {
+            pro2::Rect obj_r = obj->get_rect();
+            if (obj_r.right >= qrect.left && obj_r.left <= qrect.right &&
+                obj_r.bottom >= qrect.top && obj_r.top <= qrect.bottom) {
+                res.insert(obj);
+            }
+        }
+
+        return res;
+    }
+
+    /**
+     * @brief Clears all objects from the spatial grid.
+     * @post All internal grid cells are emptied.
+     */
+    void clear() { objects_.clear(); }
 };
