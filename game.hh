703e720319be1f498294caf6f44389d66493c0e9
Max Vilà Ruiz <max16506@gmail.com>
2025-07-17 12:26:20 +0200

    Add game state variables and UI options for pregame and endgame screens

--- game.hh
+++ game.hh
@@ -53,4 +53,45 @@ class Game {
     List<Star> stars_;                              // All Stars
     Finder<Star> star_finder_;                      // Star finder
     std::set<const Star*> star_actualObj_;          // Star in view
+
+        // Utils
+    static const std::vector<std::vector<int>> option_pointer_sprite_;
+    static const std::vector<std::vector<int>> finish_flag_sprite_;
+    std::vector<std::pair<std::string, std::vector<std::string>>> instructions_ = {
+        {"UP", {"`", "W"}}, {"DOWN", {",", "S"}}, {"LEFT", {"<", "A"}}, {"RIGHT", {">", "D"}},
+        {"(UN)PAUSE", {"P"}}, {"QUIT", {"ESC"}}, {"CONTINUE", {"RETURN"}}
+    };
+
+    // Pre-game screen
+    std::vector<std::string> pregame_options_ = {"1 PLAYER GAME", "2 PLAYER GAME"};
+    std::vector<std::string>::iterator pregame_options_it_ = pregame_options_.begin();
+    std::vector<pro2::Color> color_vec_ = {pro2::red, pro2::green, pro2::yellow, pro2::blue};
+    bool single_player_ = false;
+
+    // End-game screen
+    std::vector<std::string> endgame_options_ = {"TRY AGAIN", "MENU", "QUIT"};
+    std::vector<std::string>::iterator endgame_options_it_ = endgame_options_.begin();
+    bool win_ = false;
+
+    // Characters' immunity
+    const int immunity_interval_ = 60;              // 1s at 60fps
+    int immunity_mario_until_ = 0;
+    int immunity_luigi_until_ = 0;
+    bool immune_mario_ = false;    
+    bool immune_luigi_ = false;
+
+    // General states
+    bool finished_ = false;                         // Game over flag
+    bool paused_ = true;                            // Pause flag
+    bool pregame_ = true;                           // Pre-game state
+    bool endgame_ = false;                          // End-game state
+    bool following_cam_ = false;                    // Vertical camera tracking state
+    bool restarting_game_ = false;
+    bool day_time_ = true;                          // Background status
+    int frame_counter_ = 0;
+    static const int day_night_interval_ = 900;     // 15s at 60fps
+
+    // Screen size
+    int width_;
+    int height_;
 };
